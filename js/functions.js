c26a394e9fa8d9107583c054eff8c5619e0925a1
Python-Lettle <1071445082@qq.com>
2019-03-10 20:58:23 +0800

    bio V0.2

--- js/functions.js
+++ js/functions.js
@@ -17,19 +17,22 @@ function indexof(arr,item){
     }
 };
 function submit(){
+	log_n++
+	console.log(log_n)
 	min = Number(document.getElementById("from").value);
 	max = Number(document.getElementById("to").value);
 	if(now!=0){
 		ques = document.getElementById("que").innerHTML;
 		answ = document.getElementById("ans").value;
-		if(log_n=15){log_n=0;cleanlog();}
+		if(log_n==15){log_n=0;cleanlog();}
 		document.getElementById("logs").innerHTML += String(now) + "-" + ques + "-" + answ + "<br/>";
+		document.getElementById("answer").innerHTML = data[now-1]["ans"][0]
 	};
 	now = randomNum(min,max);
 	document.getElementById("que").innerHTML = data[now-1]["que"];
 	document.getElementById("ans").value = "";
-	log_n++
-	console.log(log_n)
+	
+	
 };
 function randomNum(minNum,maxNum){ 
     switch(arguments.length){ 
